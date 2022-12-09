e15fb5318ba4f70dc034db601c99caa1c368ba53
CanineAnimal <111728906+CanineAnimal@users.noreply.github.com>
2022-12-09 13:23:45 +1300

    Update rec.js

--- scripts/rec.js
+++ scripts/rec.js
@@ -5,7 +5,6 @@ var notify = true;
 var originalTime;
 var tem;
 var nat;
-
 function login(){
 	nat = document.querySelector('#NATION').value;
 	tem = document.querySelector('#TEMPLATE').value;
@@ -27,9 +26,7 @@ function login(){
 	}else{
 		document.body.innerHTML += '<BR/><BR/><SPAN CLASS="ERROR">Error: Nation is not an authorised recruiter for Greater Dienstad.</SPAN>'
 	}
-}
-
-function start(){
+}function start(){
 	var nations = [];
 	var request2;
 	originalTime = (new Date()).getTime();
@@ -50,16 +47,20 @@ function start(){
 	notify = document.querySelector('#SOUND').checked;
 	document.body.innerHTML = '<A CLASS="TG" TARGET="_BLANK" HREF="' + link + '" ONCLICK="recBut()">Recruit</A><BR/><BR/><INPUT TYPE="CHECKBOX" ID="SOUND"/> Notify';
 	document.querySelector('#SOUND').checked = notify;
-}
-
-function recBut(){
+}function recBut(){
 	notify = document.querySelector('#SOUND').checked;
-	document.body.innerHTML = '<BUTTON CLASS="TG" ONCLICK="generateRecruits()">Acknowledge</BUTTON><BR/><BR/><INPUT TYPE="CHECKBOX" ID="SOUND"/> Notify';
-	document.querySelector('#SOUND').checked = notify;
-}
-
-function generateRecruits(){
-	document.body.innerHTML = 'Loading...<BR/><BR/><INPUT TYPE="CHECKBOX" ID="SOUND"/> Notify'
+	if(notify){
+		document.body.innerHTML = '<BUTTON CLASS="TG" ONCLICK="generateRecruits()">Acknowledge</BUTTON><BR/><BR/><INPUT TYPE="CHECKBOX" ID="SOUND" CHECKED/> Notify';
+	}else{
+		document.body.innerHTML = '<BUTTON CLASS="TG" ONCLICK="generateRecruits()">Acknowledge</BUTTON><BR/><BR/><INPUT TYPE="CHECKBOX" ID="SOUND"/> Notify';
+	}
+}function generateRecruits(){
+	notify = document.querySelector('#SOUND').checked;
+	if(notify){
+		document.body.innerHTML = 'Loading...<BR/><BR/><INPUT TYPE="CHECKBOX" ID="SOUND" CHECKED/> Notify';
+	}else{
+		document.body.innerHTML = 'Loading...<BR/><BR/><INPUT TYPE="CHECKBOX" ID="SOUND"/> Notify';
+	}
 	originalTime2 = (new Date()).getTime();
 	var request = new XMLHttpRequest();
 	request.open('GET', 'https://www.nationstates.net/cgi-bin/api.cgi?nation=' + nat + '&q=foundedtime', false);
@@ -86,9 +87,7 @@ function generateRecruits(){
 		nats[nats.length] = nations[item];
 	}
 	setTimeout(postRecruits, originalTime2 + time * 1000 - new Date().getTime());
-}
-
-function postRecruits(){
+}function postRecruits(){
 	link += '&message=' + tem;
 	notify = document.querySelector('#SOUND').checked;
 	if(notify){
