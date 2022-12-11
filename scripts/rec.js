d71ec07688a996252ea5e7c9a92102376786c536
CanineAnimal <111728906+CanineAnimal@users.noreply.github.com>
2022-12-11 18:35:27 +1300

    Update rec.js

--- scripts/rec.js
+++ scripts/rec.js
@@ -52,7 +52,7 @@ function login(){
 				}
 			}
 		}
-		console.log(mkNat);
+		console.log(nations[item] + ':' + str(mkNat));
 		if(mkNat){link += nations[item] + ','};
 		if(nats.length == 8){break};
 	}
