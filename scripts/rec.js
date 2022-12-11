9393bb3ca6d5b2b6649b7f3f25c0775246ba56d8
CanineAnimal <111728906+CanineAnimal@users.noreply.github.com>
2022-12-11 20:44:56 +1300

    Update rec.js

--- scripts/rec.js
+++ scripts/rec.js
@@ -80,10 +80,9 @@ function generateRecruits(){
 	originalTime = (new Date()).getTime();
 	for (var item = 0; item < request2.responseXML.querySelector('NEWNATIONS').innerHTML.split(',').length; item++){
 		if(nats.indexOf(request2.responseXML.querySelector('NEWNATIONS').innerHTML.split(',')[item]) == -1){
-			nations[nations.length] = request2.responseXML.querySelector('NEWNATIONS').innerHTML.split(',')[item] + ',';
+			if(nations.length < 8){nations[nations.length] = request2.responseXML.querySelector('NEWNATIONS').innerHTML.split(',')[item]};
 			nats[nats.length] = request2.responseXML.querySelector('NEWNATIONS').innerHTML.split(',')[item];
 		}
-		if(nations.length == 8){break};
 	}if(nations.length > 0){
 		link = 'https://www.nationstates.net/page=compose_telegram?tgto=';
 		for(var item = 0; item < Math.min(8, nations.length); item++){
