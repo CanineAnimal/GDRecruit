8fe6dde2c188d797d7a220390e701cb121919603
CanineAnimal <111728906+CanineAnimal@users.noreply.github.com>
2022-12-11 16:07:35 +1300

    Update rec.js

--- scripts/rec.js
+++ scripts/rec.js
@@ -42,11 +42,16 @@ function login(){
 		nations[nations.length] = request2.responseXML.querySelector('NEWNATIONS').innerHTML.split(',')[item];
 		for(var jtem = 0; jtem <= nations[item].length/2; jtem++){
 			part = nations[item].substr(item, 0.5 + jtem + nations[item].length/2);
-			for(var ktem = 0; ktem < nats.length; ktem++){
-				if(nats[ktem].indexOf(part) == -1 || nats[ktem].length > 2 * part.length){
-					link += nations[item] + ',';
-					nats[nats.length] = nations[item];
+			if(nats.length){
+				for(var ktem = 0; ktem < nats.length; ktem++){
+					if(nats[ktem].indexOf(part) == -1 || nats[ktem].length > 2 * part.length){
+						link += nations[item] + ',';
+						nats[nats.length] = nations[item];
+					}
 				}
+			}else{
+				link += nations[item] + ',';
+					nats[nats.length] = nations[item];
 			}
 		}
 		if(nats.length = 8){break};
