e02e7ce459231bcacc82979448a13ae66f0884c6
CanineAnimal <111728906+CanineAnimal@users.noreply.github.com>
2023-01-21 20:09:42 +1300

    Update timer.js

--- scripts/timer.js
+++ scripts/timer.js
@@ -10,10 +10,10 @@ document.querySelector('BUTTON').onclick = function(){
 		var nations = regrequest.responseXML.querySelector('NATIONS').innerHTML.split(':');
 		var item = 0;
 		var interval = setInterval(function(){
-			var request = new XMLHttpRequest();
-			request.open('GET', 'https://www.nationstates.net/cgi-bin/api.cgi?nation=' + nations[item] + '&q=lastlogin&user_agent=GDRecruit maintained by the Ice States GitHub https://github.com/CanineAnimal/GDRecruit user ' + nation, false);
-			request.send();
 			if(item < nations.length){
+				var request = new XMLHttpRequest();
+				request.open('GET', 'https://www.nationstates.net/cgi-bin/api.cgi?nation=' + nations[item] + '&q=lastlogin&user_agent=GDRecruit maintained by the Ice States GitHub https://github.com/CanineAnimal/GDRecruit user ' + nation, false);
+				request.send();
 				if((((new Date()).getTime()/1000 - eval(request.responseXML.querySelector('LASTLOGIN').innerHTML) > 237600) && ((new Date()).getTime()/1000 - eval(request.responseXML.querySelector('LASTLOGIN').innerHTML) < 246240)) || ((new Date()).getTime()/1000 - eval(request.responseXML.querySelector('LASTLOGIN').innerHTML) > 5140800)){
 					document.body.querySelector('#OUTPUT').innerHTML += nations[item] + ' will CTE next update! Oh dear. <BR/>';
 				}
