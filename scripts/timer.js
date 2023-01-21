69cb2d31f73c59c06ef201b1a70f083378ba56a3
CanineAnimal <111728906+CanineAnimal@users.noreply.github.com>
2023-01-21 20:04:01 +1300

    Update timer.js

--- scripts/timer.js
+++ scripts/timer.js
@@ -5,13 +5,13 @@ document.querySelector('BUTTON').onclick = function(){
 	}else{
 		document.querySelector('#LOADING').innerHTML = 'Loading...'
 		var regrequest = new XMLHttpRequest();
-		regrequest.open('GET', 'https://www.nationstates.net/cgi-bin/api.cgi?region=greater_dienstad&q=nations&user_agent=GDRecruit by the Ice States used by ' + nation, false);
+		regrequest.open('GET', 'https://www.nationstates.net/cgi-bin/api.cgi?region=greater_dienstad&q=nations&user_agent=GDRecruit maintained by the Ice States GitHub https://github.com/CanineAnimal/GDRecruit user ' + nation, false);
 		regrequest.send();
 		var nations = regrequest.responseXML.querySelector('NATIONS').innerHTML.split(':');
 		var item = 0;
 		var interval = setInterval(function(){
 			var request = new XMLHttpRequest();
-			request.open('GET', 'https://www.nationstates.net/cgi-bin/api.cgi?nation=' + nations[item] + '&q=lastlogin&user_agent=GDRecruit by the Ice States used by ' + nation, false);
+			request.open('GET', 'https://www.nationstates.net/cgi-bin/api.cgi?nation=' + nations[item] + '&q=lastlogin&user_agent=GDRecruit maintained by the Ice States GitHub https://github.com/CanineAnimal/GDRecruit user ' + nation, false);
 			request.send();
 			if(item < nations.length){
 				if((((new Date()).getTime()/1000 - eval(request.responseXML.querySelector('LASTLOGIN').innerHTML) > 237600) && ((new Date()).getTime()/1000 - eval(request.responseXML.querySelector('LASTLOGIN').innerHTML) < 246240)) || ((new Date()).getTime()/1000 - eval(request.responseXML.querySelector('LASTLOGIN').innerHTML) > 5140800)){
