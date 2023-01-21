3b85c8e3f016ca0ac6c56a63fc9cdba701fb9cd4
CanineAnimal <111728906+CanineAnimal@users.noreply.github.com>
2023-01-21 19:51:55 +1300

    Update timer.js

--- scripts/timer.js
+++ scripts/timer.js
@@ -9,11 +9,11 @@ var interval = setInterval(function(){
 	request.send();
 	if(item < nations.length){
 		if((((new Date()).getTime()/1000 - eval(request.responseXML.querySelector('LASTLOGIN').innerHTML) > 237600) && ((new Date()).getTime()/1000 - eval(request.responseXML.querySelector('LASTLOGIN').innerHTML) < 246240)) || ((new Date()).getTime()/1000 - eval(request.responseXML.querySelector('LASTLOGIN').innerHTML) > 5140800)){
-			document.body.querySelector('OUTPUT').innerHTML += nations[item] + ' will CTE next update! Oh dear. <BR/>';
+			document.body.querySelector('#OUTPUT').innerHTML += nations[item] + ' will CTE next update! Oh dear. <BR/>';
 		}
 		item++;
 	}else{
-		document.body.querySelector('LOADING').innerHTML = 'Process complete.'
+		document.body.querySelector('#LOADING').innerHTML = 'Process complete.'
 		clearInterval(interval);
 	}
 }, 650);
