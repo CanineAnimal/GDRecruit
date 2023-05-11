cc2d06c1471eb3fa0d4874aafdddc0f924c95efa
CanineAnimal <111728906+CanineAnimal@users.noreply.github.com>
2023-05-11 18:33:51 +1200

    Update process_dumps.py
    Made changes to inter alia print the stage the program is at, capitalise nation names from dumps correctly, and output CTEs in ctes.txt

--- timer/process_dumps.py
+++ timer/process_dumps.py
@@ -3,6 +3,7 @@ import xml.etree.ElementTree as ExtraTerrestrial
 from datetime import datetime
 import requests
 import time
+import os
 
 # Get dumps already fetched
 f = open('dumps/dumps.txt', 'r')
@@ -17,10 +18,24 @@ some_major = 60 * datetime.fromtimestamp(1680498300).hour + datetime.fromtimesta
 
 # Inputs
 user_name = input('Enter your nation name: ')
-days = input('Enter amount of days for which to scan CTEs: ')
+days = int(input('Enter amount of days for which to scan CTEs: '))
 
+if len(saved_dumps) < 1 or not os.path.exists('dumps/' + saved_dumps[len(saved_dumps) - 1] + '.xml'):
+	error = BaseException('No data dump found. Use get_dumps.py to download the latest dump -- do not add them manually.')
+	raise error # Use latest dump; raise error if no dump exists
+
+print('Parsing dump...')
 nats = ExtraTerrestrial.parse(open('dumps/' + saved_dumps[len(saved_dumps) - 1] + '.xml')).findall('NATION')
+
+print('Fetching GD members...')
 gd_nations = ExtraTerrestrial.fromstring(requests.get('https://www.nationstates.net/cgi-bin/api.cgi?region=greater_dienstad&q=nations', headers={'User-Agent':'CTE timer script created by the Ice States and used by ' + user_name}).text)[0].text.split(':')
+
+print('Blanking ctes.txt file...')
+f = open('ctes.txt', 'w')
+f.write('')
+f.close()
+
+print('Checking last log-ins...')
 original_time = time.time_ns();
 for nation in nats:
 	# Find next major update
@@ -31,7 +46,7 @@ for nation in nats:
 			break
 		item += 1
 	try:
-		gd_nations.index(nation[0].text)
+		gd_nations.index(nation[0].text.lower().replace(' ', '_'))
 		cont = True
 	except:
 		cont = False
@@ -46,6 +61,9 @@ for nation in nats:
 			actual_last_login = eval(ExtraTerrestrial.fromstring(requests.get('https://www.nationstates.net/cgi-bin/api.cgi?nation=' + nation[0].text + '&q=lastlogin', headers={'User-Agent':'CTE timer script created by the Ice States and used by ' + user_name}).text)[0].text)
 			if(next_major - actual_last_login > (28 - (days - 1)) * 86400 and next_major - actual_last_login < (29 - (days - 1)) * 86400):
 				print(nation[0].text + ' will CTE by next major update.')
+				f = open('ctes.txt', 'a')
+				f.write(nation[0].text + ' will CTE by next major update.\n')
+				f.close()
 		
 		if(next_major - last_login > (60 - (days - 1)) * 86400 and next_major - last_login < (61 - (days - 1)) * 86400):
 			# Nation is on vacation mode and may CTE; check that nation has not been logged into since dump
@@ -54,3 +72,8 @@ for nation in nats:
 			actual_last_login = eval(ExtraTerrestrial.fromstring(requests.get('https://www.nationstates.net/cgi-bin/api.cgi?nation=' + nation[0].text + '&q=lastlogin', headers={'User-Agent':'CTE timer script created by the Ice States and used by ' + user_name}).text)[0].text)
 			if(next_major - actual_last_login > (28 - days) * 86400 and next_major - actual_last_login < (29 - days) * 86400):
 				print(nation[0].text + ' will CTE by next major update, and is on vacation mode.')
+				f = open('ctes.txt', 'a')
+				f.write(nation[0].text + ' will CTE by next major update, and is on vacation mode.\n')
+				f.close()
+
+print('Process complete.')
